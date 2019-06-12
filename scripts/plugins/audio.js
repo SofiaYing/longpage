4acee2f4be4bbd974010a8b10edb23e5ed50ed10
zhangyy.dz <zhangyyfangzheng@163.com>
2019-06-12 10:20:12 +0800

    t

--- scripts/plugins/audio.js
+++ scripts/plugins/audio.js
@@ -32,9 +32,10 @@
             }
             var data = eval('(' + jsonnode.value + ')');
             this.data = data;
-            if (isLongPage) {
-                observeAudioAutoPlay.observe($(demo).parent()[0])
-            } else if (data.playOnPageTurn === "true" && isHasSpecParents.length === 0) {
+            // if (isLongPage) {
+            //     observeAudioAutoPlay.observe($(demo).parent()[0])
+            // } else 
+            if (data.playOnPageTurn === "true" && isHasSpecParents.length === 0) {
                 function autoPlay() {
                     var playRes = window.playAgentAudio(demo);
                     if (!playRes) return;
@@ -135,38 +136,38 @@
         }
     }
 
-    var observeAudioAutoPlay = new IntersectionObserver((entries) => {
-        entries.forEach((item, index) => {
-            var demo = $(item.target).children('audio')[0]
+    // var observeAudioAutoPlay = new IntersectionObserver((entries) => {
+    //     entries.forEach((item, index) => {
+    //         var demo = $(item.target).children('audio')[0]
 
-            var data = dataProcess(demo)
-            var child0 = GetChild('', item.target, 0);
-            var child1 = GetChild('', item.target, 1);
-            var isHasSpecParents = parents("div[title='PopupContent'][title='Animation']", demo);
+    //         var data = dataProcess(demo)
+    //         var child0 = GetChild('', item.target, 0);
+    //         var child1 = GetChild('', item.target, 1);
+    //         var isHasSpecParents = parents("div[title='PopupContent'][title='Animation']", demo);
 
-            if (item.isIntersecting) {
-                if (data.playOnPageTurn === "true" && isHasSpecParents.length === 0) {
-                    // function autoPlay() {
-                    var playRes = window.playAgentAudio(demo);
-                    if (!playRes) return;
-                    child0.style.display = 'none';
-                    child1.style.display = 'inline';
-                    // }
-                    // if (data.playDelay > 0) {
-                    //     setTimeout(autoPlay, data.playDelay);
-                    // } else {
-                    //     autoPlay()
-                    // }
-                }
-            } else {
-                window.pauseAgentAudio(demo);
-                // 下一次重新开始播放
-                demo.currentTime = 0;
-                child0.style.display = 'inline';
-                child1.style.display = 'none';
-            }
-        })
-    });
+    //         if (item.isIntersecting) {
+    //             if (data.playOnPageTurn === "true" && isHasSpecParents.length === 0) {
+    //                 // function autoPlay() {
+    //                 var playRes = window.playAgentAudio(demo);
+    //                 if (!playRes) return;
+    //                 child0.style.display = 'none';
+    //                 child1.style.display = 'inline';
+    //                 // }
+    //                 // if (data.playDelay > 0) {
+    //                 //     setTimeout(autoPlay, data.playDelay);
+    //                 // } else {
+    //                 //     autoPlay()
+    //                 // }
+    //             }
+    //         } else {
+    //             window.pauseAgentAudio(demo);
+    //             // 下一次重新开始播放
+    //             demo.currentTime = 0;
+    //             child0.style.display = 'inline';
+    //             child1.style.display = 'none';
+    //         }
+    //     })
+    // });
 
     function dataProcess(node) {
         var inputNode = $(node).parent().children('input')
