93efa509087cc4637749017d93f908e3b4697cf5
zhangyy.dz <zhangyyfangzheng@163.com>
2019-08-15 16:25:31 +0800

    长页翻页

---
+++ scripts/options.js
@@ -0,0 +1,25 @@
+﻿var fx_options = {
+"0": [{container: "buttonItem-6",plugin: "button",option: {}},
+],
+};
+(function () {
+window.FX = window.FX || {};
+var paths = [
+'core/interface.js',
+'core/fx.js',
+'core/utils.js',
+'plugins/button.js',
+],
+baseURL = './scripts/';
+for (var i = 0, pi; pi = paths[i++];) {
+var extension = pi.split('.').pop();
+if(extension === 'css'){
+document.write('<link rel="stylesheet" type="text/css" href="' + baseURL + pi  +'"></link>');
+}
+else{
+document.write('<script type="text/javascript" src="' + baseURL + pi + '?timestamp=' +
+FX.version +
+'"></script>');
+}
+}
+})();
