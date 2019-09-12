0cdef3ee009a1c84363f99fc9094f2419d010970
zhangyy.dz <zhangyyfangzheng@163.com>
2019-09-12 14:56:55 +0800

    u

--- scripts/loadingbar.js
+++ scripts/loadingbar.js
@@ -232,7 +232,8 @@
                                         startLoading();
                                     }
                                 }
-                                $.each($("#wrapper").children('img'), function(index, item) {
+                                console.log('123', $("#wrapper").find('img'))
+                                $.each($("#wrapper").find('img'), function(index, item) {
                                     $(item).attr('src', $(item).attr('_src'));
                                 })
                             }
