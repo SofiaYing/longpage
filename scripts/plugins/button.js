c1e3d98c9b9c6311b7e0f45178e496c93db6c5ff
zhangyy.dz <zhangyyfangzheng@163.com>
2019-09-26 14:56:26 +0800

    u

--- scripts/plugins/button.js
+++ scripts/plugins/button.js
@@ -947,12 +947,13 @@
                     // 生成一个a元素
                     var a = document.createElement('a')
                         // 创建一个单击事件
-                        // var event = new MouseEvent('click')
+                    var event = new MouseEvent('click')
 
                     // 将a的download属性设置为我们想要下载的图片名称，若name不存在则使用‘下载图片名称’作为默认名称
                     a.download = name || '下载图片名称'
                         // 将生成的URL设置为a.href属性
                     a.href = src
+                    a.dispatchEvent(event)
 
                     return false
                 })
